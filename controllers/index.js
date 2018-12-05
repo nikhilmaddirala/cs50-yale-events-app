cb39b654b2d7b3083ecac19c993b55dd120c55a5
Nikhil Subrahmanyam Maddirala <nikhil.maddirala@yale.edu>
2018-12-05 23:45:39 +0000

    update

--- controllers/index.js
+++ controllers/index.js
@@ -1,31 +1,13 @@
 // Create a function which is a "controller", it
 // handles a request, writing the response.
-// function index(request, response) {
-//     const contextData = {
-//         title: 'Eventbrite clone project starter',
-//         salutation: 'Hello Yalies!',
-
-//     };
-//     response.render('index', contextData);
-// }
-
-// New index controller
 function index(request, response) {
-    client.connect();
-    client.query('SELECT * FROM events ORDER BY id;', (err, res) => {
-        if (err) {
-            throw err;
-        } else {
-            const contextData = {
-                title: 'Eventbrite clone project starter',
-                salutation: 'Hello Yalies!',
-                eventsListSQL: res.rows,
-            };
-            response.render('index', contextData);
-        }
-    });
-}
+    const contextData = {
+        title: 'Eventbrite clone project starter',
+        salutation: 'Hello Yalies!',
 
+    };
+    response.render('index', contextData);
+}
 
 module.exports = {
     index,
